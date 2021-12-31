34de29dbf726ddc49e3649978b1a3f1524b90472
fiantyogalihp <fiantiogalih@gmail.com>
2021-12-31 11:04:37 +0700

    menambahkan iterasi js

--- Js/script.js
+++ Js/script.js
@@ -2,28 +2,29 @@ var x = 10;
 console.log('hello world');
 console.log('nilai dari variable x adalah ' + x);
 
-alert('selamat datang');
-var tryAgain = true;
+var lanjutan = confirm('selamat datang\n tekan "cancel" tour singkat\n tekan "OK" tour panjang');
+if (lanjutan === true) {
+  var tryAgain = true;
+  while (tryAgain) {
+    var nama = prompt('masukkan nama anda: ');
+    alert('hello ' + nama);
 
-while (tryAgain) {
-  var nama = prompt('masukkan nama anda: ');
-  alert('hello ' + nama);
+    tryAgain = confirm('mw coba lagi??');
+    // karena variable try bersifat true, jika try berganti menjadi false, maka pengulangan akan berhenti 
 
-  tryAgain = confirm('mw coba lagi??');
-  // karena variable try bersifat true, jika try berganti menjadi false, maka pengulangan akan berhenti 
+    alert('terima kasih telah berkunjung!!');
+  }
 }
-
-alert('terima kasih telah berkunjung!!');
 
 var jmlAngkot = 10;
 angkotBeroperasi = 6;
 noAngkot = 1;
 
-while (noAngkot <= angkotBeroperasi) {
-  console.log('Angkot no. ' + noAngkot + ' beroperasi dengan baik');
-  noAngkot++;
-}
-
-for (let noAngkot = 7; noAngkot <= 10; noAngkot++) {
-  console.log('Angkot no. ' + noAngkot + ' tidak beroperasi')
+// penggunaan perulangan dengan iterasi IF ELSE
+for (let noAngkot = 1; noAngkot <= 10; noAngkot++) {
+  if (noAngkot <= 6) {
+    console.log('Angkot no. ' + noAngkot + ' beroperasi dengan baik');
+  } else {
+    console.log('Angkot no. ' + noAngkot + ' tidak beroperasi');
+  }
 }
